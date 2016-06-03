fda257eb4d68cb5c9025ad6c931d6bed5bd09003
Christian Siber <siber.christian@gmail.com>
2016-06-03 15:50:56 +0200

    Minor changes to custom LuisModelAttribute

---
+++ LevisBot/Attributes/MyLuisModelAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Bot.Builder.Luis;
+using System;
+using System.Configuration;
+
+namespace LevisBot.Attributes
+{
+	[Serializable]
+	public class MyLuisModelAttribute : LuisModelAttribute
+	{
+		/// <summary>
+		/// Custom LuisModel that inherits from the Microsoft LuisModel.
+		/// This was implemented to extract the AppId and SubscriptionKey from the Codebase.
+		/// </summary>
+		public MyLuisModelAttribute() : base(
+			ConfigurationManager.AppSettings["LuisModelId"],
+			ConfigurationManager.AppSettings["LuisSubscriptionKey"])
+		{
+		}
+	}
+}
