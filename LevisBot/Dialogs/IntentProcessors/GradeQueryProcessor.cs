8120f27633c3a4789d0a1bb04f42a4f6027ff8f1
Christian Siber <siber.christian@gmail.com>
2016-06-05 14:50:51 +0200

    GradeQueryProcessor implemented for processing GradeQuery intents.

---
+++ LevisBot/Dialogs/IntentProcessors/GradeQueryProcessor.cs
@@ -0,0 +1,25 @@
+﻿using LevisBot.Attributes;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace LevisBot.Dialogs.IntentProcessors
+{
+  [Serializable]
+  public class GradeQueryProcessor : BaseIntentProcessor
+  {
+    [RequiredEntity("Course",
+                    PromptMessage = "For what course do you want to know the grade ?")]
+    public string Course { get; set; }
+
+    public GradeQueryProcessor(LuisResult luisResult, Action<IDialogContext> defaultSuspendAction) : base(luisResult, defaultSuspendAction)
+    {
+    }
+
+    protected override async Task Process(IDialogContext context)
+    {
+      await context.PostAsync($"Your grade in {Course} is: ...");
+    }
+  }
+}
