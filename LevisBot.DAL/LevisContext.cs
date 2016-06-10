fbe3df301edae448ff2dd48df5b506038efba354
Christian Siber <siber.christian@gmail.com>
2016-06-10 15:49:10 +0200

    Levis context and initializer added

---
+++ LevisBot.DAL/LevisContext.cs
@@ -0,0 +1,19 @@
+﻿using LevisBot.DAL.DAO;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace LevisBot.DAL
+{
+  public class LevisContext : DbContext
+  {
+    public LevisContext() : base("LevisDb")
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Grade> Grades { get; set; }
+    public DbSet<Course> Courses { get; set; }
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder) => modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+  }
+}
---
+++ LevisBot.DAL/LevisInitializer.cs
@@ -0,0 +1,38 @@
+﻿using LevisBot.DAL.DAO;
+using System.Data.Entity;
+
+namespace LevisBot.DAL
+{
+  public class LevisInitializer : DropCreateDatabaseAlways<LevisContext>
+  {
+    protected override void Seed(LevisContext context)
+    {
+      var user = new User { FirstName = "Jamie", LastName = "Vardy" };
+      context.Users.Add(user);
+
+      var sve = new Course { Name = "SVE" };
+      var mus = new Course { Name = "MUS" };
+      context.Courses.Add(sve);
+      context.Courses.Add(mus);
+
+      context.Grades.Add(new Grade
+      {
+        Course = sve,
+        Result = 20,
+        Number = 1,
+        Type = GradeableType.Assignment,
+        MaxResult = 20,
+        Student = user
+      });
+
+      context.Grades.Add(new Grade
+      {
+        Course = mus,
+        Result = 1,
+        Student = user
+      });
+
+      context.SaveChanges();
+    }
+  }
+}
